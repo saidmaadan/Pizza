3ff2318e80a498b4f35cab59171e8be7f6ba4ef9
Said Maadan <saidmaadan@gmail.com>
2014-04-23 15:12:44 -0500

    Failing test for pizza.topping.size method

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -14,7 +14,13 @@ describe Pizza do
 
       expect(pizza.toppings).to eq(toppings)
     end
-  end
+    it 'defaults the toppings to cheese only, if the pizza has no toppings' do
+  pizza = Pizza.new
+
+  expect(pizza.toppings.size).to eq(1)
+  expect(pizza.toppings.first.name).to eq('cheese')
+end
+end
 end
 
 describe Topping do
