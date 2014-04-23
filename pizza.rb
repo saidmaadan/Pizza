fa24c8b506a93338db622e9d2d66274273512bee
Said Maadan <saidmaadan@gmail.com>
2014-04-23 16:46:02 -0500

    Modified pizza.rb

--- pizza.rb
+++ pizza.rb
@@ -4,12 +4,30 @@ class Pizza
   def initialize(toppings = [Topping.new('cheese', vegetarian: true)])
     @toppings = toppings
   end
+         # Alternative method by Mike
+  # def initialize(*toppings)
+  #   if toppings ==[]
+  #     @toppings = [Topping.new("cheese")]
+  #   else
+  #     @toppings = toppings
+  #   end
+  # end
 
   def vegetarian?
     @toppings.all? do |topping|
       topping.vegetarian == true
     end
   end
+
+       # Alternative method by Mike
+  # def vegetarian?
+  #   @toppings.each do |topping|
+  #     if topping.vegetarian != true
+  #       return false
+  #     end
+  #   end
+  #   true
+
 
   def add_topping(toppings)
     @toppings << toppings
